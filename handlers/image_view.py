96b835e36f4cec96db1ca938239890f9c0f3508f
tyk <yongketian@gmail.com>
2015-01-06 16:44:51 +0800

    实现接口imageView/2,imageView/3,imageView/4,imageView/5

--- handlers/image_view.py
+++ handlers/image_view.py
@@ -46,23 +46,22 @@ class ImageViewHandler(BaseImageHandler):
 
             size = im.size
             if "0"   == mode:
-                if w and int(w) > size[0]:
-                    w = size[0]
-                if h and int(h) > size[1]:
-                    h = size[1]
+                if not w or not h: # 暂时返回原图，后期改为参数错误
+                    self.write_image(im, file_name, ext)
+                    return
+                long_edge = max(size)
+                short_edge = min(size)
 
-                if w and h:
-                    pass
-                elif w:
-                    w = int(w)
-                    h = int(size[1] * (w / size[0]))
-                elif h:
-                    h = int(h)
-                    w = int(size[0] * (h / size[1]))
-                else:
-                    pass
+                w = int(w)
+                h = int(h)
 
-                im = im.resize((w, h))
+                ratio_long = w / long_edge
+                ratio_short = h / short_edge
+
+                ratio = min(ratio_long, ratio_short)
+                resize = tuple(int(x * ratio) for x in size)
+
+                im = im.resize(tuple(resize))
                 self.write_image(im, file_name, ext)
             elif "1" == mode:
                 if not w and not h: # 暂时返回原图，后期改为参数错误
@@ -111,13 +110,85 @@ class ImageViewHandler(BaseImageHandler):
                 self.write_image(im, file_name, ext)
 
             elif "2" == mode:
-                pass
+                if not w and not h: # 暂时返回原图，后期改为参数错误
+                    self.write_image(im, file_name, ext)
+                    return
+
+                if not w:
+                    h = int(h)
+                    w = int(size[0] * (h / size[1]))
+                if not h:
+                    w = int(w)
+                    h = int(size[1] * (w / size[0]))
+                w = int(w)
+                h = int(h)
+
+                im = im.resize((w, h))
+                self.write_image(im, file_name, ext)
             elif "3" == mode:
-                pass
+                if not w or not h: # 暂时返回原图，后期改为参数错误
+                    self.write_image(im, file_name, ext)
+                    return
+
+                w = int(w)
+                h = int(h)
+
+                ratio_w = ratio_h = 1
+                if w:
+                    ratio_w = w / size[0]
+                if h:
+                    ratio_h = h / size[1]
+
+                resize = []
+                if ratio_w > ratio_h:
+                    resize = tuple(int(x * ratio_w) for x in size)
+                else:
+                    resize = tuple(int(x * ratio_h) for x in size)
+
+                im = im.resize(tuple(resize))
+                self.write_image(im, file_name, ext)
+
             elif "4" == mode:
-                pass
+                if not w or not h: # 暂时返回原图，后期改为参数错误
+                    self.write_image(im, file_name, ext)
+                    return
+                long_edge = max(size)
+                short_edge = min(size)
+
+                w = int(w)
+                h = int(h)
+
+                ratio_long = w / long_edge
+                ratio_short = h / short_edge
+
+                ratio = max(ratio_long, ratio_short)
+                resize = tuple(int(x * ratio) for x in size)
+
+                im = im.resize(tuple(resize))
+                self.write_image(im, file_name, ext)
+
             elif "5" == mode:
-                pass
+                if not w or not h: # 暂时返回原图，后期改为参数错误
+                    self.write_image(im, file_name, ext)
+                    return
+                long_edge = max(size)
+                short_edge = min(size)
+                w = int(w)
+                h = int(h)
+
+                ratio_long = w / long_edge
+                ratio_short = h / short_edge
+                ratio = max(ratio_long, ratio_short)
+                resize = tuple(int(x * ratio) for x in size)
+
+                box = [0, resize[0], resize[1], 0]
+                box[0] = int((resize[0] - long_edge) / 2)
+                box[2] = long_edge + box[0]
+                box[1] = int((resize[1] - short_edge) / 2)
+                box[3] = short_edge + box[1]
+
+                im = im.resize(resize).crop(tuple(box))
+                self.write_image(im, file_name, ext)
             else:
                 self.write_image(im, file_name, ext)
         elif EXIF       == interface:
