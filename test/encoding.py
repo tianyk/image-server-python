602cf78beb6b80702a369291c84baf0176581c68
tyk <yongketian@gmail.com>
2015-01-27 23:43:41 +0800

    编码测试

---
+++ test/encoding.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__author__ = 'tyk'
+
+import urllib
+
+text = u'黑体字'
+
+print urllib.quote(text.encode('utf-8'))
+
+text = u'%E9%BB%91%E4%BD%93%E5%AD%97'
+
+print type(urllib.unquote(text))
+
+print urllib.unquote(text).decode('utf-8')
